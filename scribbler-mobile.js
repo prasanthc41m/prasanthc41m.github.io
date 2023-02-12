a6493b83e6df1d343fbb608f9aca1ec940bfcb44
Prasanth c41m <58906808+prasanthc41m@users.noreply.github.com>
2023-02-12 15:24:20 +0530

    Update scribbler-mobile.js

--- scribbler-mobile.js
+++ scribbler-mobile.js
@@ -13,41 +13,42 @@ var getAll = function (selector, scope) {
 if (document.getElementsByClassName('demo').length > 0) {
   var i = 0;
   var txt = `whoami
-\A seasoned and accomplished cybersecurity professional with over a decade of experience in the IT industry with 3 years of dedicated experience in cybersecurity
+\   A seasoned and accomplished cybersecurity professional with over a decade of experience in the IT industry with 3 years of dedicated experience in cybersecurity.
 
 Achievements:
-- Contributed to the identification and mitigation of a critical vulnerability (XSS) in Codologic's forum application, Codoforum
-- Published finding on Portswigger Web Security's website on January 13, 2020 titled "Codoforum software patched against stored XSS vulnerability"
+ Contributed to the identification and mitigation of a critical vulnerability (XSS) in Codologic's forum application, Codoforum
 
-- The MITRE Corporation [CVE-2020-5842] 
-– Codoforum 4.8.3 allows XSS in the user registration page which allows to compromise administrators 
+Published finding on Portswigger Web Security's website on January 13, 2020 titled "Codoforum software patched against stored XSS vulnerability"
 
-- Exploit Database [EDB-ID: 47876] 
-– Codoforum 4.8.3 – Persistent Cross-Site Scripting
+ The MITRE Corporation [CVE-2020-5842] 
+   Codoforum 4.8.3 allows XSS in the user registration page which allows to compromise administrators 
 
-- Exploit Database [GHDB-ID: 5689] 
-– Google Dork Description: intext:”powered by codoforum”inurl:”/user/login”  
+ Exploit Database [EDB-ID: 47876] 
+   Codoforum 4.8.3 – Persistent Cross-Site Scripting
+
+ Exploit Database [GHDB-ID: 5689] 
+   Google Dork Description: intext:”powered by codoforum”inurl:”/user/login”  
 
 Volunteer Experience:
-- Volunteer at Kerala Police Cyberdome (2020 - Present)
-- Chapter Leader at OWASP Kannur Chapter (2021 - Present)
+ Volunteer at Kerala Police Cyberdome (2020 - Present)
+ Chapter Leader at OWASP Kannur Chapter (2021 - Present)
 
 Training and Certifications:
-- EC-Council Certified Security Analyst (ECSA V9) - Feb 2019 - Feb 2025
-- Certified Security Analyst (CSA) - 2018
-- Apple Certified Support Professional 10.7 (ACSP) - Jul 2012
-- Certified Arcos PAM Implementor (CAPI)
-- Cisco Certified Network Associate (CCNA)
-- Microsoft Certified Systems Engineer (MCSE)
+ EC-Council Certified Security Analyst (ECSA V9) - Feb 2019 - Feb 2025
+ Certified Security Analyst (CSA) - 2018
+ Apple Certified Support Professional 10.7 (ACSP) - Jul 2012
+ Certified Arcos PAM Implementor (CAPI)
+ Cisco Certified Network Associate (CCNA)
+ Microsoft Certified Systems Engineer (MCSE)
 
 Conferences:
-- Organized multiple events as a Chapter Leader at OWASP Kannur Chapter in 2022
-- Provided technical support at the Cyber Security Summit 2021 "Real Time Real Attack" hosted by Cyberdome Kozhikode
-- Organized Retrain-2021, Adversary Emulation - A Practical Approach with eHackify Cybersecurity Research & Trainings
-- Provided technical support at Redteam Hacker Academy's Redteam Security Summit-2018, Cyber Security & Hacking Conference
+ Organized multiple events as a Chapter Leader at OWASP Kannur Chapter in 2022
+ Provided technical support at the Cyber Security Summit 2021 "Real Time Real Attack" hosted by Cyberdome Kozhikode
+ Organized Retrain-2021, Adversary Emulation - A Practical Approach with eHackify Cybersecurity Research & Trainings
+ Provided technical support at Redteam Hacker Academy's Redteam Security Summit-2018, Cyber Security & Hacking Conference
 
 Interests:
-- Reading blogs and articles, bug-hunting, creating CTFs, supporting and contributing to the cybersecurity community, and more.\
+ Reading blogs and articles, bug-hunting, creating CTFs, supporting and contributing to the cybersecurity community, and more.\
   `;
   var speed = 25;
 
